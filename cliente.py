4c33c885d43ead0891107c4337a811c39c6a1162
void-computer <116570548+void-computer@users.noreply.github.com>
2023-01-05 07:22:04 +0000

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,49 @@
+import socket
+import threading
+
+# Choosing Server and Nickname
+wserver = int(input("Choose your server type 1 for server 1 \n type 2 for server 2 \n and type 3 for server 3"))
+if wserver == 1:
+    portnumber = 35555
+
+if wserver == 2:
+    portnumber = 45555
+
+if wserver == 3:
+    portnumber = 55555
+
+nickname = input("Choose your nickname: ")
+
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', portnumber))
+
+# Listening to Server and Sending Nickname
+def receive():
+    while True:
+        try:
+            # Receive Message From Server
+            # If 'NICK' Send Nickname
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            # Close Connection When Error
+            print("An error occured!")
+            client.close()
+            break
+
+# Sending Messages To Server
+def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
+
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
